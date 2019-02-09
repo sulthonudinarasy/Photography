4740bf5fa5a68d785bccf38982ea93c095ae133d
sulthonarasy <sulthon.arasy@gmail.com>
2019-02-09 14:13:43 +0700

    -  mengubah tampilan - menambahkan progresbar ke dalam xml activty main - method memunculkan atau menghilangkan loading

--- app/src/main/java/com/sulthon/photography/MainActivity.java
+++ app/src/main/java/com/sulthon/photography/MainActivity.java
@@ -4,14 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout relativeLoadiing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        relativeLoadiing = findViewById(R.id.relative_loading);
         RecyclerView recycler = findViewById(R.id.recycler);
         recycler.setLayoutManager(new LinearLayoutManager(this));
 
@@ -20,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
 
         recycler.setAdapter(adapter);
 
+    }
+    private void showLoading(){
+        relativeLoadiing.setVisibility(View.VISIBLE);
+
+    }
+    private void hideLoading(){
+
+        relativeLoadiing.setVisibility(View.GONE);
     }
 
 }
